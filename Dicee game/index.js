f239a3dbb95c44a3f320ec6149a5b42466a0a367
Sanskar Jain <78869626+SanskarJain007@users.noreply.github.com>
2022-12-28 17:03:24 +0530

    Add files via upload

---
+++ Dicee game/index.js
@@ -0,0 +1,26 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice" + randomNumber1+".png";
+
+var randomImageSource = "images/"+randomDiceImage;
+
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+var randomImageSource2="images/dice"+randomNumber2+".png";
+
+var image2=document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h2").innerHTML="🏆🥇Player 1 Wins!!";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h2").innerHTML="Player 2 wins!!🏆🥇";
+}
+else{
+    document.querySelector("h2").innerHTML="🏆It's a tie🏆"; 
+}
